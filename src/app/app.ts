4e68db5e07e1b3c0a6465402f4fe80d503621ded
lydiano <andrianina.lydiano@gmail.com>
2025-06-27 18:14:52 +0300

    update app component structure and remove unused tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameControls } from './features/labyrinth/components/game-controls/game-controls';
+import { LabyrinthBoard } from './features/labyrinth/components/labyrinth-board/labyrinth-board';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameControls, LabyrinthBoard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'pertimm-labyrinthe';
 }
